f969e6dfbf95716d555a01958cf288f901c73b0f
Rob O'Doherty <rodoherty1@gmail.com>
2014-06-10 22:38:59 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Coursera - Functional Programming in Scala
+==========================================
+
+Some notes and experiments from Week 6 of Coursera's course on Functional Programming in Scala.
+
+The N-Queens problem introduces **for-expressions** to solve a combinatorial search problem. 
+
+The ScalaTest includes some basic tests to demonstrate Ranges, Vector.forall, for-expressions and Zip.
+
+
---
+++ src/main/scala/week6/NQueens.scala
@@ -0,0 +1,29 @@
+package week6
+
+object NQueens {
+    
+    def nqueens(n: Int) : Set[List[Int]] = {
+        def isSafe(col: Int, queens: List[Int]): Boolean = {
+            val row = queens.length
+            
+            val queensWithRow = (row-1 to 0 by -1) zip queens
+
+            queensWithRow forall {
+                case (r, c) => col != c && math.abs(col - c) != row - r
+            }
+        }
+	            
+        def placeQueens(k: Int): Set[List[Int]] =
+	        if (k == 0) Set(List())
+	        else {
+	            for {
+	                queens <- placeQueens(k-1)
+	                col <- 0 until n
+	                if isSafe(col, queens)                
+	            } yield col :: queens
+	        }
+
+	    placeQueens(n)
+    }
+
+}
